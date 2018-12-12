530eb4acbc812220e67a84c7707aa5aa357be73f
Illiah <Drawmesky@gmail.com>
2018-12-12 23:20:44 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict"; // - удалет ошибку старого стандарта
+
+
+
+/*//масив объектов
+
+
+
+let persone = {
+    name: "Jonh",
+    age: "25",
+    isMarried: false
+};
+
+console.log(persone.name);
+console.log(persone["age"]);
+
+//масив и елементы масива
+
+let arr = ["plump.png", "orange.jpg", "apple.webp"];
+console.log(arr[0]);
+
+//команды
+
+alert("hi sweety");
+
+//let answer = confirm("where are u are?");
+//console.log(answer);
+
+
+// typeof - тыпы данных
+//let answer = prompt("where are u are?", "here");
+//console.log(typeof (null));
+
+
+//операторы
+
+//console.log("arr" + "-object");
+//console.log(4+ "-object");
+
+//унарный +prompt превоащает строку в число
+let answer = +prompt("where are u are?", "here");
+console.log(typeof (answer));
+
+console.log(4+ +"-object");
+*/
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log("2" == 2);
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+console.log(isChecked || isClose);
+console.log(isChecked && isClose);
+console.log(isChecked || !isClose);
